720969d0f69e68e3ad8a2ef58ac7dee1fca54923
Proletcultist <zenin2606@gmail.com>
2026-09-17 10:29:37 +0700

    [lab1] Fix formatting

---
+++ lab1/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("com.diffplug.spotless") version "8.10.2"
+}
+
+subprojects {
+   apply(plugin = "com.diffplug.spotless")
+
+    spotless{
+        java{
+            googleJavaFormat()
+        }
+    }
+}
--- lab1/util/src/main/java/ru/nsu/zenin/util/InetSocketAddressParser.java
+++ lab1/util/src/main/java/ru/nsu/zenin/util/InetSocketAddressParser.java
@@ -1,25 +1,25 @@
 package ru.nsu.zenin.util;
 
+import java.net.InetSocketAddress;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.net.InetSocketAddress;
 
 public class InetSocketAddressParser {
-    private static final Pattern ADDRESS_PATTERN = Pattern.compile("([^:]+):(\\d+)");
+  private static final Pattern ADDRESS_PATTERN = Pattern.compile("([^:]+):(\\d+)");
 
-    private InetSocketAddressParser() {}
+  private InetSocketAddressParser() {}
 
-    public static InetSocketAddress parse(String str) throws IllegalArgumentException {
-        Matcher m = ADDRESS_PATTERN.matcher(str);
-        if (!m.matches()) {
-            throw new IllegalArgumentException("Invalid format of ip + port string");
-        }
-
-        try {
-            int port = Integer.parseInt(m.group(2));
-            return new InetSocketAddress(m.group(1), port);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Invalid port string");
-        }
+  public static InetSocketAddress parse(String str) throws IllegalArgumentException {
+    Matcher m = ADDRESS_PATTERN.matcher(str);
+    if (!m.matches()) {
+      throw new IllegalArgumentException("Invalid format of ip + port string");
     }
+
+    try {
+      int port = Integer.parseInt(m.group(2));
+      return new InetSocketAddress(m.group(1), port);
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException("Invalid port string");
+    }
+  }
 }
