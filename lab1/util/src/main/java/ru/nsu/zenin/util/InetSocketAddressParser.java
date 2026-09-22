ecf07170437504cf2ddb1fa89814ab06a4b1e2fe
Proletcultist <zenin2606@gmail.com>
2026-09-22 22:40:04 +0700

    [lab1] Refactor keygen server

--- lab1/util/src/main/java/ru/nsu/zenin/util/InetSocketAddressParser.java
+++ lab1/util/src/main/java/ru/nsu/zenin/util/InetSocketAddressParser.java
@@ -10,6 +10,7 @@ public class InetSocketAddressParser {
     private InetSocketAddressParser() {}
 
     public static InetSocketAddress parse(String str) throws IllegalArgumentException {
+        // TODO: Throw some checked exception istead of IllegalArgumentException
         Matcher m = ADDRESS_PATTERN.matcher(str);
         if (!m.matches()) {
             throw new IllegalArgumentException("Invalid format of ip + port string");
