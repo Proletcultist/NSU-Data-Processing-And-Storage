1098d4b8d53dc2af631febf7d62c1bddf7fbfeee
Proletcultist <zenin2606@gmail.com>
2026-09-23 15:01:18 +0700

    [lab1] Move to java 27 (spotless plugin is broken for now)

--- lab1/server/build.gradle.kts
+++ lab1/server/build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
 // Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(26)
+        languageVersion = JavaLanguageVersion.of(27)
     }
 }
 
